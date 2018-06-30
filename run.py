84f9c71a4865c1536b1580b5cf84c7b260a42bb7
Araali Farooq <farooqp35h@gmail.com>
2018-07-01 01:06:53 +0300

    Add database management file

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app.db import dbManager
 dbUtils = dbManager.DBConnection()
 
 if __name__ == "__main__":
-	dbUtils.create_tables()
-	#app.run(debug=True)
+	#dbUtils.create_tables()
+	app.run(debug=True)
